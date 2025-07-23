be33aa172c477bebffdb580e44561a88e5b083e4
chadmcrowell <chadmcrowell@gmail.com>
2025-07-23 16:48:15 +0200

    update readme

--- test-latency-latam.sh
+++ test-latency-latam.sh
@@ -3,18 +3,31 @@
 ENDPOINT="http://172.233.4.110/api/healthz"
 HOST_HEADER="api.myapp.lat"
 
-declare -A PROBES=(
-  [brazil_sao_paulo]="curlip.com"
-  [chile_santiago]="curlip.com"
-  [colombia_bogota]="curlip.com"
-  [mexico_mexico_city]="curlip.com"
-  [argentina_buenos_aires]="curlip.com"
-  [usa_dallas]="curlip.com"
+# Define regions and endpoints as parallel arrays
+REGIONS=(
+  "Brazil_Sao_Paulo"
+  "Chile_Santiago"
+  "Colombia_Bogota"
+  "Mexico_MexicoCity"
+  "Argentina_BuenosAires"
+  "USA_Dallas"
 )
 
-for region in "${!PROBES[@]}"; do
-  echo "🌎 Testing from: $region"
-  curl -s -H "Host: $HOST_HEADER" --resolve "$HOST_HEADER:80:$ENDPOINT" "http://${PROBES[$region]}/curl?url=$ENDPOINT" \
+PROXIES=(
+  "curlip.com"
+  "curlip.com"
+  "curlip.com"
+  "curlip.com"
+  "curlip.com"
+  "curlip.com"
+)
+
+for i in "${!REGIONS[@]}"; do
+  REGION="${REGIONS[$i]}"
+  PROXY="${PROXIES[$i]}"
+
+  echo "🌎 Testing from: $REGION"
+  curl -s -H "Host: $HOST_HEADER" "http://${PROXY}/curl?url=$ENDPOINT" \
     -w "\nConnect: %{time_connect}s\nTotal: %{time_total}s\n" \
     -o /dev/null
   echo "---------------------------------------------"
